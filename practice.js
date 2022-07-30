7ecf8169dcdfe03badba624c3b9ac4c176330b41
rosadev <noblerosakim@gmail.com>
2022-07-30 23:10:37 +0900

    add get & post ajax

--- practice.js
+++ practice.js
@@ -1,13 +1,13 @@
 var 출석부 = ['흥민', '영희', '철수', '재석'];
 
 function 이름찾기(a){
+  Boolean = '철수';
   출석부.forEach(function(name){
     if (name == a){
       console.log('있어요');
     } else {
       console.log('없어요');
     }
-    return;
   });
 }
 
