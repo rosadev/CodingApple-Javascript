b339bd472a05d7dac8aa3a22704e78db1811cd56
rosadev <noblerosakim@gmail.com>
2022-07-30 22:49:04 +0900

    add forEach practice

---
+++ practice.js
@@ -0,0 +1,15 @@
+var 출석부 = ['흥민', '영희', '철수', '재석'];
+
+function 이름찾기(a){
+  출석부.forEach(function(name){
+    if (name == a){
+      console.log('있어요');
+    } else {
+      console.log('없어요');
+    }
+    return;
+  });
+}
+
+// 이름찾기('철수');
+이름찾기('명수');
--- tab.js
+++ tab.js
@@ -34,3 +34,45 @@ var car2 = {name : '소나타', price : [50000, 3000, 4000]}  // object형
 document.querySelector('.car-title').innerHTML = car2.name;
 document.querySelector('.car-price').innerHTML = car2.price[0];
 
+
+// 셔츠나 바지를 옵션 선택하면 사이즈 셀렉터가 나타나게 하기
+$('.form-select').eq(0).on('input', function(){
+  var item = $('.form-select').eq(0).val();  /* e.currentTarget이나 this를 써줘도 됨 */
+  if( item == '셔츠' ){
+    $('.form-select').eq(1).removeClass('size');
+    $('.form-select').eq(1).html('');
+    shirts.forEach(function(data){
+      $('.form-select').eq(1).append(`<option>${data}</option>`)
+    })
+  } else if (item == '바지') {
+    $('.form-select').eq(1).removeClass('size');
+    $('.form-select').eq(1).html('');
+    /*  forEach : array에 붙일 수 있는 반복문 */
+    pants.forEach(function(data){
+      $('.form-select').eq(1).append(`<option>${data}</option>`)
+    });
+  } else {
+    $('.form-select').eq(1).addClass('size');    
+  }
+});
+
+// 서버에서 보낸 데이터 가정
+var pants = [28, 30, 32, 34];
+var shirts = [90, 95, 100, 105];
+
+// object 반복문 돌리기 (for in  반복문)
+var obj = {name : 'kim', age : 20}
+
+for (var key in obj) {
+  // console.log(key);  // 키를 꺼낼때
+  console.log(obj[key]);  // 값을 꺼낼때
+}
+
+// HTML 생성법
+var a = document.createElement('p');
+a.innerHTML = '안녕';
+document.querySelector('#test').appendChild(a);
+
+// HTML 생성법 2
+var template ='<p>안녕</p>';
+document.querySelector('#test').insertAdjacentHTML('beforeend', template);
